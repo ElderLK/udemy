30a548e9c4bc931260baaea60219a69b514da254
Elder Kuss <elder.kuss@wisereducacao.com>
2019-10-16 15:04:38 -0300

    first step

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component'
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(user => this.setState({monsters: user}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList name="Elder">
+        {
+          this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
+        </CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
