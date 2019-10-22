73e71bd9c52a2a839ab700384cc4eb35440dbfd0
Elder Kuss <elder.kuss@wisereducacao.com>
2019-10-22 19:30:06 -0300

    Updates

---
+++ precache-manifest.ffbe2ddbbff4aa0172ac56c7e195ce16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8f528483224876d871eab9de835316",
+    "url": "/udemy/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3d47f666cba311a5f35b",
+    "url": "/udemy/monsters-rolodex/static/css/main.289909f5.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/udemy/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "3d47f666cba311a5f35b",
+    "url": "/udemy/monsters-rolodex/static/js/main.f8546ec0.chunk.js"
+  },
+  {
+    "revision": "8c8a816fe38ed88c6c10",
+    "url": "/udemy/monsters-rolodex/static/js/runtime-main.f8a7c254.js"
+  }
+]);
