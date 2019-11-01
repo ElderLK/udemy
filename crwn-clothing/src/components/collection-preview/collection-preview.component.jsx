62b6b01a1bfaf598283e0d4cdd0f1ebaf32fd42f
Elder Kuss <elder.kuss@wisereducacao.com>
2019-11-01 12:35:57 -0300

    add collection data

---
+++ crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './collection-preview.component.scss';
+
+export default class preview-collection extends Component {
+  render() {
+    return <div />;
+  }
+}
