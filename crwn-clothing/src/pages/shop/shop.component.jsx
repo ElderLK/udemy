628c93a6851da9a4ec9c8076bd61d32c176c529f
ElderWiser <elder.kuss@wisereducacao.com>
2019-12-10 23:54:23 -0300

    add new component to store in redux

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-export default class ShopPage extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-    return (
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+const ShopPage = ({collections}) => 
+     (
       <div className="shop-page">
-        {
-          collections
-          .filter((item, idx) => idx < 5)
-          .map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
+        <CollectionsOverview />
       </div>
     );
-  }
-}
+
+export default ShopPage;
