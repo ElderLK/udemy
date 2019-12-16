7d531b8657d834c2c08b7c15315e56d992194d92
ElderWiser <elder.kuss@wisereducacao.com>
2019-12-15 22:51:49 -0300

    add stripe integration

--- crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import './checkout.styles.scss';
@@ -33,6 +35,12 @@ const CheckoutPage = ( {cartItems, total} ) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Credit Card Test: 
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123 
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
