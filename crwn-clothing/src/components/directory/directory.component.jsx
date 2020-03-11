59e5f9f98a31e6b5c5c12d54259301db038f6f7e
Elder Kuss <elderkuss@MacBook-Pro-de-Elder.local>
2020-03-11 09:46:18 -0300

    Home office.

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { useDispatch, useSelector } from "react-redux";
 import { selectDirectorySections } from '../../redux/directory/directory.selector';
 import MenuItem from '../menu-item/menu-item.component'
 import './directory.styles.scss';
 
-const Directory = ({sections}) => 
-     (
+const Directory = () => {
+  const sections = useSelector(selectDirectorySections);
+  return (
       <div className="directory-menu">
         {
           sections.map(({id ,...otherSectionProps}) => (
@@ -15,9 +15,6 @@ const Directory = ({sections}) =>
         }
       </div>
     )
+}
 
-const mapStateToProps =  createStructuredSelector({
-  sections: selectDirectorySections
-});
-
-export default connect(mapStateToProps)(Directory);
+export default Directory;
