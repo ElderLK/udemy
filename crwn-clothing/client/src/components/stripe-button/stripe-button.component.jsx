203f984273acbfc132adc246a0a7fc6702b71e8f
ElderWiser <elder.kuss@wisereducacao.com>
2020-05-11 22:46:38 -0300

    add express to backend

--- crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKEy = 'pk_test_ecavYjfT4zZpyDxpgLO1Z2Kd00l5mu61Rh';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Success');
+        axios({ 
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then( resp => {
+            alert('Payment successful')
+        }).catch( err => {
+            console.log('Payment error', JSON.parse(err));
+            alert('There was a issue with your payment!. Please sure you use the correct card');
+        })
     }
 
     return (<StripeCheckout 
