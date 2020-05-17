1b1d4c73b16c156d1ad234866689e7752ca81c98
ElderWiser <elder.kuss@wisereducacao.com>
2020-05-16 21:26:47 -0300

    remove redux

--- crwn-clothing-context/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing-context/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,9 @@
 import React, { useContext } from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import { selectCartItems } from '../../redux/cart/cart.selectors';
-import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
+
 
 import { CartContext } from '../../providers/cart/cart.provider';
 
@@ -36,8 +33,6 @@ const CartDropdown = ({ history }) => {
     </div>
   );
 }
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
-});
 
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+
+export default withRouter((CartDropdown));
