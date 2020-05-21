56a41041891e62e87fa7d5ea9af8a25b28d4be83
ElderWiser <elder.kuss@wisereducacao.com>
2020-05-21 15:00:39 -0300

    finish graphql setup

--- crwn-clothing-graphQL/src/index.js
+++ crwn-clothing-graphQL/src/index.js
@@ -6,12 +6,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
+import { typeDefs, resolvers } from './graphql/resolvers';
 
 const httpLink = createHttpLink({
   uri: 'https://crwn-clothing.com/'
@@ -21,8 +22,18 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
     link: httpLink,
-    cache
+    cache,
+    typeDefs, 
+    resolvers
 });
+
+client.writeData({
+  data: {
+    cartHidden: true,
+    cartItems: [],
+    itemCount: 0
+  }
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
